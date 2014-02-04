4dee2cce143f49417cdcf994745ef39c7e63c138
Tommi Keskitalo <tommi.keskitalo@ideja.fi>
2014-02-04 12:26:40 +0200

    First
    Kaikki peruskamat asetettuna

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Työhakemus">
+        <meta name="author" content="Tommi Keskitalo">
+
+        <title>työpaikka hakemus</title>
+
+        <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+ 
+        <!-- Add custom CSS here -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/custom.css" rel="stylesheet">
+
+    </head>
+    <body>
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Hakemus</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="#about"><i class="fa fa-home"></i> About</a></li>
+                    <li><a href="#form"><i class="fa fa-pencil-square-o"></i> Form</a></li>
+                    <li><a href="#contact"><i class="fa fa-envelope-o"></i> Contact</a></li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    <div class="hero-unit">
+        <div class="container">
+            <h1>Karhukopla - Paras kesätyö</h1>
+        </div>
+    </div>
+    <div class="container">
+        <div class="page-header">
+            <h1 id="about">About</a> <small>Työnkuva</small></h1>
+        </div>
+
+        <p>Karhukoplassa työskennellessäsi tulet ylläpitämään Metropolian tietotekniikan koulutusohjelman käyttämiä pilvipalveluita, kuten <a href="https://harjoittelu.edu.metropolia.fi/">Harjoittelu</a>, <a href="http://games.metropolia.fi/">Games</a>, <a href="https://webinssi.edu.metropolia.fi/login.php">Webinssi</a> ja monia muita. Osa ylläpidettävistä järjestelmistä on peräisin koululle oppilaiden kehittämien projekti- tai opinnäytetöiden tuloksena, näissä tilanteissa oppilaan valmistuttua kehitystyön jatkaminen on siirtynyt Karhukoplan vastuulle.</p>
+        <p>Työtehtäviisi kuuluu myös täysin uusien sovellusten kehittäminen ja suunnittelu, joiden ideat tulevat lähinnä opettajilla tai heidän pyynnöstään. Karhukopla tarjoaa sinulle laajan mahdollisuuden kehittää omaa osaamistasi muun muassa <a href="http://fi.wikipedia.org/wiki/Drupal">Drupal -sisällönhallintajärjestelmän</a>, <a href="http://fi.wikipedia.org/wiki/Wordpress">Wordpress -julkaisualustan</a>, <a href="http://symfony.com/">Symfony -PHP sovelluskehyksen</a> parissa ja tarjoaa sinulle modernit työkalut ja kehitysympäristön.</p>
+        
+        <h3>Vaativuus ja haasteet</h3>
+        <p>Tarvitset työssäsi laajaa osaamista erilaisista sovelluskehitys tekniikoista.</p>
+        <ul>
+            <li>Projektinhallinta (Agile / Jira) ja dokumentointi
+                <ul>
+                    <li>Ketterät kehitysmenetelmät</li>
+                    <li>Koodin katselmointi & suunnittelu</li>
+                </ul>
+            </li>
+
+            <li>Keskitetty koodinhallinta (Continuous Integration / SVN)
+                <ul>
+                    <li>CI-palvelimen käyttö & versionhallinta</li>
+                    <li>Buildien hallinta ja raportointi (ja niiden ymmärtäminen)</li>
+                </ul>
+            </li>
+
+            <li>Palvelinylläpito
+                <ul>
+                    <li>Windows- ja Linux-palvelin</li>
+                </ul>
+            </li>
+
+            <li>Testausympäristö (Symfony2 ja Zend -projektit)
+                <ul>
+                    <li>PHPunit, testien kirjoittaminen ja ajaminen</li>
+                </ul>
+            </li>
+
+            <li>Legacy-ylläpito
+                <ul>
+                    <li>PHP-versioiden päivittäminen</li>
+                    <li>Vanhan koodin refaktorointia ja aukkojen paikkaamista</li>
+                </ul>
+            </li>
+
+            <li>Käyttöliittymät
+                <ul>
+                    <li>Javascript, Web grafiikka, layout-suunnitelu</li>
+                </ul>
+            </li>
+
+            <li>Olio-ohjelmointi
+                <ul>
+                    <li>TTD, Olio-keskitetty ohjelmistokehykset</li>
+                </ul>
+            </li>
+        </ul>
+
+        <h3>Etsimme:</h3>
+        <p>Henkilöä joka on motivoitunut itse opiskelemaan ja kehittämään itsestään ammattitaitoisen ohjelmoijan. Oletamme kuitenkin hakijan osaavan ohjelmoinnin perusteet ja peruskäsitteet.</p>
+
+
+        <div class="page-header">
+            <h1 id="form">Form</a> <small>Työhakemuslomake</small></h1>
+        </div>
+    </div>
+    </body>
+    <script src="bower_components/jquery/jquery.min.js"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+</html>
