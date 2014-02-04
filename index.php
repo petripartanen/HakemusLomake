158412d6abc9a51409b29ba3ef40c403748be18d
Tommi <mato12@hotmail.fi>
2014-02-04 13:55:18 +0200

    Poistettu listaus ja muutettu koko pöskä dl/dt/dd-malliseksi. Lisäksi lisätty sisältöä Footeriin ja formiin

--- index.php
+++ index.php
@@ -48,108 +48,133 @@
     </div>
     <div class="container">
         <div class="page-header">
-            <h1 id="about">About</a> <small>Työnkuva</small></h1>
+            <h1 id="about">About <small>Työnkuva</small></h1>
         </div>
 
         <p>Karhukoplassa työskennellessäsi tulet ylläpitämään Metropolian tietotekniikan koulutusohjelman käyttämiä pilvipalveluita, kuten <a href="https://harjoittelu.edu.metropolia.fi/">Harjoittelu</a>, <a href="http://games.metropolia.fi/">Games</a>, <a href="https://webinssi.edu.metropolia.fi/login.php">Webinssi</a> ja monia muita. Osa ylläpidettävistä järjestelmistä on peräisin koululle oppilaiden kehittämien projekti- tai opinnäytetöiden tuloksena, näissä tilanteissa oppilaan valmistuttua kehitystyön jatkaminen on siirtynyt Karhukoplan vastuulle.</p>
         <p>Työtehtäviisi kuuluu myös täysin uusien sovellusten kehittäminen ja suunnittelu, joiden ideat tulevat lähinnä opettajilla tai heidän pyynnöstään. Karhukopla tarjoaa sinulle laajan mahdollisuuden kehittää omaa osaamistasi muun muassa <a href="http://fi.wikipedia.org/wiki/Drupal">Drupal -sisällönhallintajärjestelmän</a>, <a href="http://fi.wikipedia.org/wiki/Wordpress">Wordpress -julkaisualustan</a>, <a href="http://symfony.com/">Symfony -PHP sovelluskehyksen</a> parissa ja tarjoaa sinulle modernit työkalut ja kehitysympäristön.</p>
-        
-        <h3>Vaativuus ja haasteet</h3>
-        <p>Tarvitset työssäsi laajaa osaamista erilaisista sovelluskehitys tekniikoista.</p>
-        <ul>
-            <li>Projektinhallinta (Agile / Jira) ja dokumentointi
-                <ul>
-                    <li>Ketterät kehitysmenetelmät</li>
-                    <li>Koodin katselmointi & suunnittelu</li>
-                </ul>
-            </li>
-
-            <li>Keskitetty koodinhallinta (Continuous Integration / SVN)
-                <ul>
-                    <li>CI-palvelimen käyttö & versionhallinta</li>
-                    <li>Buildien hallinta ja raportointi (ja niiden ymmärtäminen)</li>
-                </ul>
-            </li>
-
-            <li>Palvelinylläpito
-                <ul>
-                    <li>Windows- ja Linux-palvelin</li>
-                </ul>
-            </li>
-
-            <li>Testausympäristö (Symfony2 ja Zend -projektit)
-                <ul>
-                    <li>PHPunit, testien kirjoittaminen ja ajaminen</li>
-                </ul>
-            </li>
-
-            <li>Legacy-ylläpito
-                <ul>
-                    <li>PHP-versioiden päivittäminen</li>
-                    <li>Vanhan koodin refaktorointia ja aukkojen paikkaamista</li>
-                </ul>
-            </li>
-
-            <li>Käyttöliittymät
-                <ul>
-                    <li>Javascript, Web grafiikka, layout-suunnitelu</li>
-                </ul>
-            </li>
-
-            <li>Olio-ohjelmointi
-                <ul>
-                    <li>TTD, Olio-keskitetty ohjelmistokehykset</li>
-                </ul>
-            </li>
-        </ul>
-
-        <dl class="dl-horizontal">
-            <dt>Jira</dt>
-            <dd>Ketterät kehitysmenetelmät, Koodin katselmointi & suunnittelu <br/>... ja dokumentointi</dd>
-        </dl>
-        <dl class="dl-horizontal">
-            <dt>SVN</dt>
-            <dd>Keskitetty koodinhallinta, Continuous Integration / SVN <br />
-                CI-palvelimen käyttö & versionhallinta. Buildien hallinta ja raportointi (ja niiden ymmärtäminen)</dd>
-        </dl>
-        <dl class="dl-horizontal">
-            <dt>Palvelinylläpito</dt>
-            <dd>Windows- ja Linux-palvelin</dd>
-        </dl>
-        <dl class="dl-horizontal">
-            <dt>Symfony2</dt>
-            <dd>Testausympäristö (Symfony2 ja Zend -projektit): PHPunit, testien kirjoittaminen ja ajaminen</dd>
-        </dl>
 
         <h3>Etsimme:</h3>
         <p>Henkilöä joka on motivoitunut itse opiskelemaan ja kehittämään itsestään ammattitaitoisen ohjelmoijan. Oletamme kuitenkin hakijan osaavan ohjelmoinnin perusteet ja peruskäsitteet.</p>
 
+        <h3>Vaativuus ja haasteet</h3>
+        <p>Tarvitset työssäsi laajaa osaamista erilaisista sovelluskehitys tekniikoista.</p>
+
+        <dl class="dl-horizontal">
+            <dt>Jira</dt>
+            <dd>Ketterät kehitysmenetelmät, Koodin katselmointi, suunnittelu <br/>... ja dokumentointi</dd>
+        </dl>
+        <dl class="dl-horizontal">
+            <dt>SVN/Git</dt>
+            <dd>Keskitetty koodinhallinta, Continuous Integration / SVN /Git <br />
+                CI-palvelimen käyttö & versionhallinta. Buildien hallinta ja raportointi (ja niiden ymmärtäminen)</dd>
+        </dl>
+        <dl class="dl-horizontal">
+            <dt>Palvelinylläpito</dt>
+            <dd>Karhukoplalla on omia palvelimia: CI-pavelin, kehityspavelin ja eristetty tuotantoympäristö</dd>
+        </dl>
+        <dl class="dl-horizontal">
+            <dt>Symfony2</dt>
+            <dd>Uusien projektien kanssa olemme käyttäneet hyvien ohjelmistotapojen mukaisia käytäntöjä. <br />
+                Testausympäristö (Symfony2 ja Zend -projektit): PHPunit, testien kirjoittaminen ja ajaminen</dd>
+        </dl>
+
+        <dl class="dl-horizontal">
+            <dt>Lecacy-koodi</dt>
+            <dd>Lecacy järjestelmien ylläpito vaatii MySQL-osaamista ja perus funktionaalista PHP:tä</dd>
+        </dl>
+
+        <dl class="dl-horizontal">
+            <dt>Käyttöliittymät</dt>
+            <dd>Javascript, grafiikka, layout-suunnitelu</dd>
+        </dl>
+
+        <dl class="dl-horizontal">
+            <dt>Julkaisualustat</dt>
+            <dd>Wordpress, drupal ja Joomla</dd>
+        </dl>
 
         <div class="page-header">
-            <h1 id="form">Form</a> <small>Työhakemuslomake</small></h1>
+            <h1 id="form">Form <small>Työhakemuslomake</small></h1>
         </div>
+
         <form role="form">
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+            <h3>Yhteystiedot</h3>
+
+            <div class="row">
+                <div class="col-sm-6 form-group">
+                    <label for="fullName">Nimesi:</label>
+                    <input type="text" class="form-control" id="fullName" name="fullName" placeholder="Anna Nimi">
+                </div>
+                <div class="col-sm-6 form-group">
+                    <label for="group">Luokkatunnus:</label>
+                    <input type="email" class="form-control" id="group" name="group" placeholder="Anna luokkatunnus">
+                </div>
             </div>
-                <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+
+            <div class="row">
+                <div class="col-sm-6 form-group">
+                    <label for="email">Sähköposti:</label>
+                    <input type="text" class="form-control" id="email" name="email" placeholder="Anna sähköposti">
+                </div>
+                <div class="col-sm-6 form-group">
+                    <label for="studentId">Opiskelijanumerosi:</label>
+                    <input type="email" class="form-control" id="studentId" placeholder="Anna opiskelijanumero">
+                </div>
             </div>
-            <div class="form-group">
-                <label for="exampleInputFile">File input</label>
-                <input type="file" id="exampleInputFile">
-                <p class="help-block">Example block-level help text here.</p>
+
+            <h3>Yleistä</h3>
+            <div class="row">
+                <div class="col-lg-12 form-group">
+                    <label for="hobbies">Harrastukset:</label>
+                    <textarea class="form-control" rows="3" id="hobbies" name="hobbies"></textarea>
+                </div>
             </div>
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox"> Check me out
-                </label>
+            <div class="row">
+                <div class="col-lg-12 form-group">
+                    <label for="studies">Aiemmat opinnot:</label>
+                    <textarea class="form-control" rows="3" id="studies" name="studies"></textarea>
+                </div>
             </div>
-            <button type="submit" class="btn btn-default">Submit</button>
+            <div class="row">
+                <div class="col-lg-12 form-group">
+                    <label for="skills">Erikoisosaaminen:</label>
+                    <textarea class="form-control" rows="3" id="skills" name="skills"></textarea>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-12 form-group">
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox"> Varmistan, että antamani tiedot pitävät paikkansa.
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-12 text-right">
+                    <button type="submit" class="btn btn-default">Lähetä hakemus</button>
+                </div>
+            </div>
         </form>
     </div>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <h4 id="contact">Karhukopla</h4>
+                    <p><i class="fa fa-envelope-o"></i>: karhukopla@metropolia.fi</p>
+                    <p>Office: Vanha maantie 6, 02600 Espoo<br />2nd. floor, room A2130</p>
+                </div>
+                <div class="col-sm-6 text-right">
+                    copyright &copy Tommi Keskitalo, 2014
+                </div>
+            </div>
+        </div>
+    </footer>
     </body>
     <script src="bower_components/jquery/jquery.min.js"></script>
     <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
