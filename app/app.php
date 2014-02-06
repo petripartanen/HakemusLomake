97f6fb86ffd10dc48b31efcd3279100598a2c695
Tommi <mato12@hotmail.fi>
2014-02-06 17:02:24 +0200

    Layout paloiteltu twig-tiedostoihin ja tehty formin käsittelylle oma controlleri

--- app/app.php
+++ app/app.php
@@ -6,9 +6,13 @@ $app = new Silex\Application();
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/../src/My/resources/views',
 ));
+$app['debug'] = true;
 
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('layout.html.twig');
+    return $app['twig']->render('home/index.html.twig');
+});
+$app->post('/create/', function () use ($app) {
+    return $app['twig']->render('create/success.html.twig');
 });
 
 return $app;
