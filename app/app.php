2b6eb53815dbdfe8de2f3c26c9027c333c905ef9
Tommi <mato12@hotmail.fi>
2014-02-07 17:43:06 +0200

    Toteutettu validoinnit. Käyttöliittymä puuttuu

--- app/app.php
+++ app/app.php
@@ -5,30 +5,42 @@ use My\Provider\FormProvider;
 /* defines $app variable as silex application */
 require_once __DIR__ . '/bootstrap.php';
 
-$app->get('/', function () use ($app) {
+$app->get('/', function (Request $request) use ($app) {
 
     $formProvider = new FormProvider();
     $applicationForm = $formProvider->getApplicationForm($app['form.factory']);
-    return $app['twig']->render('home/index.html.twig', array(
-        'applicationForm' => $applicationForm->createView()
-    ));
-});
 
-$app->post('/submit/', function (Request $request) use ($app) {
-    $formProvider = new FormProvider();
-    $applicationForm = $formProvider->getApplicationForm($app['form.factory']);
     $applicationForm->handleRequest($request);
 
     if ($applicationForm->isValid()) {
         $data = $applicationForm->getData();
 
         return $app->redirect('/success/');
-    } else {
-        var_dump($applicationForm->isValid());
-
     }
-    exit;
-    return $app['twig']->render('create/success.html.twig');
+    else {
+        return $app['twig']->render('home/index.html.twig', array(
+            'applicationForm' => $applicationForm->createView()
+        ));
+    }
+});
+
+$app->match('/fill/', function (Request $request) use ($app) {
+
+    $formProvider = new FormProvider();
+    $applicationForm = $formProvider->getApplicationForm($app['form.factory']);
+
+    $applicationForm->handleRequest($request);
+
+    if ($applicationForm->isValid()) {
+        $data = $applicationForm->getData();
+
+        return $app->redirect('/success/');
+    }
+    else {
+        return $app['twig']->render('fill/index.html.twig', array(
+            'applicationForm' => $applicationForm->createView()
+        ));
+    }
 });
 
 $app->get('/success/', function () use ($app) {
--- src/My/Provider/formProvider.php
+++ src/My/Provider/formProvider.php
@@ -7,59 +7,113 @@ class FormProvider {
     public function getApplicationForm($factory) {
         return $factory->createBuilder('form')
             ->add('fullName', 'text', array(
-                'label' => 'Koko nimi'
+                'label' => 'Koko nimi',
+                'constraints' => array(
+                    new Assert\NotBlank()
+                )
             ))
             ->add('group', 'text', array(
-                'label' => 'Ryhmätunnus'
+                'label' => 'Ryhmätunnus',
+                'constraints' => array(
+                    new Assert\NotBlank()
+                )
             ))
             ->add('userid', 'text', array(
-                'label' => 'Tuubitunnus'
+                'label' => 'Tuubitunnus',
+                'constraints' => array(
+                    new Assert\NotBlank()
+                )
             ))
             ->add('email','email', array(
                 'label' => 'Sähköpostiosoite',
-                'constraints' => new Assert\Email()
+                'constraints' => array(
+                    new Assert\NotBlank(),
+                    new Assert\Email()
+                )
             ))
             ->add('studies', 'textarea', array(
-                'label' => 'Opinnot'
+                'label' => 'Opinnot',
+                'constraints' => array(
+                    new Assert\NotBlank()
+                )
             ))
             ->add('experiences', 'textarea', array(
-                'label' => 'Web-ohjelmointi kokemukset'
+                'label' => 'Web-ohjelmointi kokemukset',
+                'constraints' => array(
+                    new Assert\NotBlank()
+                )
             ))
             ->add('alternativeExperiences', 'textarea', array(
-                'label' => 'Muu työkokemus & projektiosaaminen'
+                'label' => 'Muu työkokemus & projektiosaaminen',
+                'constraints' => array(
+                    new Assert\NotBlank()
+                )
             ))
             ->add('programming', 'textarea', array(
-                'label' => 'Mitä on ohjelmointi?'
+                'label' => 'Mitä on ohjelmointi?',
+                'constraints' => array(
+                    new Assert\NotBlank()
+                )
             ))
             ->add('meAsProgrammer', 'textarea', array(
-                'label' => 'Millaisena sovelluskehittäjänä näet itsesi?'
+                'label' => 'Millaisena sovelluskehittäjänä näet itsesi?',
+                'constraints' => array(
+                    new Assert\NotBlank()
+                )
             ))
             ->add('skills', 'textarea', array(
-                'label' => 'Muu osaaminen ja taidot'
+                'label' => 'Muu osaaminen ja taidot',
+                'constraints' => array(
+                    new Assert\NotBlank()
+                )
             ))
             ->add('whyWorkHere', 'textarea', array(
-                'label' => 'Miksi haluat Metropolialle töihin?'
+                'label' => 'Miksi haluat Metropolialle töihin?',
+                'constraints' => array(
+                    new Assert\NotBlank()
+                )
             ))
             ->add('also', 'textarea', array(
-                'label' => 'Mitä halaisit vielä kertoa tai sanoa?'
+                'label' => 'Mitä halaisit vielä kertoa tai sanoa?',
+                'constraints' => array(
+                    new Assert\NotBlank()
+                )
             ))
             ->add('linkedIn', 'text', array(
-                'required' => false
+                'required' => false,
+                'constraints' => array(
+                    new Assert\Url()
+                )
             ))
             ->add('facebook', 'text', array(
-                'required' => false
+                'required' => false,
+                'constraints' => array(
+                    new Assert\Url()
+                )
             ))
             ->add('twitter', 'text', array(
-                'required' => false
+                'required' => false,
+                'constraints' => array(
+                    new Assert\Url()
+                )
             ))
             ->add('bitbucket', 'text', array(
-                'required' => false
+                'required' => false,
+                'constraints' => array(
+                    new Assert\Url()
+                )
             ))
             ->add('github', 'text', array(
-                'required' => false
+                'required' => false,
+                'constraints' => array(
+                    new Assert\Url()
+                )
             ))
             ->add('stackOverflow', 'text', array(
-                'required' => false
+                'required' => false,
+                'constraints' => array(
+                    new Assert\Url()
+                )
             ))
             ->getForm();
     }
